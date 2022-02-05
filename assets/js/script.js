ceae7692950451a280714df3eb6f57ffc3031aaf
Jashcraft <ashcraft.jonathon@gmail.com>
2022-02-05 13:11:31 -0700

    update script.js
    added missing closing tag

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,7 @@ submitBtnEl.addEventListener('click', function (event) {
         .then(function(data){
             console.log(data);
         });
+});
 
 for (var i = 0; i < states.length; i++) {
     var optionEl = document.createElement("option")
